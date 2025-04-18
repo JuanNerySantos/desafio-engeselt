030f3a5a82a28aaefd94f39959b974ab5c1df6bf
Juan <juannnerysantos@gmail.com>
2025-04-18 13:42:12 -0300

    chore: add initial setup

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backgroundColor: Color.fromARGB(255, 248, 187, 208),
+          foregroundColor: Color.fromARGB(
+            255,
+            106,
+            66,
+            38,
+          ), // cor do texto e ícones
+        ),
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text("DoceHub")));
+  }
+}
