39734d4a598f791004e6e176ed7d189e88ec2ba3
Juan <juannnerysantos@gmail.com>
2025-04-18 15:38:36 -0300

    refactor: split components into separete files

---
+++ lib/my_app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:marketplace/pages/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backgroundColor: Color.fromARGB(255, 248, 187, 208),
+          foregroundColor: Color.fromARGB(
+            255,
+            106,
+            66,
+            38,
+          ), // cor do texto e ícones
+        ),
+      ),
+      home: HomePage(),
+    );
+  }
+}
