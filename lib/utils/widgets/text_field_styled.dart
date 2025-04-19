e372fcc5975d9db35e8e0c7571f168ad3b11b353
Juan <juannnerysantos@gmail.com>
2025-04-19 18:59:20 -0300

    chore: add custom reusable widgets to textField

---
+++ lib/utils/widgets/text_field_styled.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class TextFieldStyled extends StatefulWidget {
+  final String hintText;
+  final IconData icon;
+  final IconData? suffixIcon;
+
+  const TextFieldStyled({
+    super.key,
+    required this.hintText,
+    required this.icon,
+    this.suffixIcon,
+  });
+
+  @override
+  State<TextFieldStyled> createState() => _TextFieldStyledState();
+}
+
+class _TextFieldStyledState extends State<TextFieldStyled> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 30),
+      child: TextField(
+        obscureText: true,
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Color(0x3FF4D16F),
+          hintText: widget.hintText,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30),
+            borderSide: BorderSide.none,
+          ),
+          prefixIcon: Icon(widget.icon, color: Color(0xff6b4226)),
+          suffixIcon:
+              widget.suffixIcon != null
+                  ? Icon(widget.suffixIcon, color: Color(0xff6b4226))
+                  : null,
+        ),
+      ),
+    );
+  }
+}
