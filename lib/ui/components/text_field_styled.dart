177338581042089c5d7ed9d99b50aa2fed1b66ff
Juan <juannnerysantos@gmail.com>
2025-04-21 20:49:39 -0300

    refactor: create function to class TextFieldStyled

--- lib/ui/components/text_field_styled.dart
+++ lib/ui/components/text_field_styled.dart
@@ -1,43 +1,41 @@
 import 'package:flutter/material.dart';
 
-class TextFieldStyled extends StatefulWidget {
+class TextFieldStyled extends StatelessWidget {
   final String hintText;
   final IconData icon;
   final IconData? suffixIcon;
   final bool obscureText;
+  final TextEditingController? controller;
 
   const TextFieldStyled({
     super.key,
     required this.hintText,
     required this.icon,
     required this.obscureText,
+    required this.controller,
     this.suffixIcon,
   });
 
-  @override
-  State<TextFieldStyled> createState() => _TextFieldStyledState();
-}
-
-class _TextFieldStyledState extends State<TextFieldStyled> {
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.symmetric(horizontal: 30),
       child: TextField(
-        obscureText: widget.obscureText,
+        controller: controller,
+        obscureText: obscureText,
         decoration: InputDecoration(
           filled: true,
-          fillColor: Color.fromARGB(176, 244, 209, 111),
-          hintText: widget.hintText,
+          fillColor: Color(0xB5F4D16F),
+          hintText: hintText,
+          prefixIcon: Icon(icon, color: Color(0xff6b4226)),
+          suffixIcon:
+              suffixIcon != null
+                  ? Icon(suffixIcon, color: Color(0xff6b4226))
+                  : null,
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(30),
             borderSide: BorderSide.none,
           ),
-          prefixIcon: Icon(widget.icon, color: Color(0xff6b4226)),
-          suffixIcon:
-              widget.suffixIcon != null
-                  ? Icon(widget.suffixIcon, color: Color(0xff6b4226))
-                  : null,
         ),
       ),
     );
