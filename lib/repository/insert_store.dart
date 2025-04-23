bdc1487091d2687cf77271b5ecb2c6873d6b134d
Juan <juannnerysantos@gmail.com>
2025-04-23 20:38:01 -0300

    feat: add InsertStoreReository to insert values in database

---
+++ lib/repository/insert_store.dart
@@ -0,0 +1,50 @@
+import 'package:drift/drift.dart';
+import 'package:marketplace/data/drift/db_connection/app_db.dart';
+
+class InsertStoreReository {
+  final String nameStore;
+  final String email;
+  final String latlong;
+  final String cep;
+  final String street;
+  final String city;
+  final String neighborhood;
+  final String password;
+  final String phone;
+  final String selectedState;
+
+  InsertStoreReository({
+    required this.nameStore,
+    required this.email,
+    required this.latlong,
+    required this.cep,
+    required this.street,
+    required this.city,
+    required this.neighborhood,
+    required this.password,
+    required this.phone,
+    required this.selectedState,
+  });
+
+  Future<bool> insertStoreDb() async {
+    final connectionDb = AppDb();
+
+    await connectionDb
+        .into(connectionDb.storeData)
+        .insert(
+          StoreDataCompanion(
+            nameStore: Value(nameStore),
+            cep: Value(cep),
+            city: Value(city),
+            email: Value(email),
+            latlong: Value(latlong),
+            neighborhood: Value(neighborhood),
+            password: Value(password),
+            phone: Value(phone),
+            selectedState: Value(selectedState),
+            street: Value(street),
+          ),
+        );
+    return true;
+  }
+}
