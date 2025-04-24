f7cc9238b5592f17d78a2e3f2978c993ce4a51da
Juan <juannnerysantos@gmail.com>
2025-04-24 00:56:14 -0300

    feat: add validation email and password ValidateLoginService

--- lib/services/validate_login.dart
+++ lib/services/validate_login.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:marketplace/repository/get_store_by_email.dart';
 import 'package:marketplace/utils/error/saffold_messenger_error.dart';
 import 'package:marketplace/utils/validation/validator_email.dart';
-import 'package:marketplace/utils/validation/validator_password.dart';
 
 class ValidateLoginService {
   String email;
@@ -14,13 +14,17 @@ class ValidateLoginService {
     required this.context,
   });
 
-  bool validatorLogin() {
+  Future<bool> validatorLogin() async {
     final email = this.email;
     final password = this.password;
     try {
       validatorEmail(email);
 
-      validatorPassword(password);
+      final store = await getStoreByEmailRepository(email);
+
+      if (store?.password.toString() != password) {
+        throw Exception("E-mail ou senha está incorreto");
+      }
 
       return true;
     } catch (e) {
