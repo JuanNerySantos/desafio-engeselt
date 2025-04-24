d7106fe85b3a8fad7416e37f10fa719041ab9478
Juan <juannnerysantos@gmail.com>
2025-04-24 00:53:33 -0300

    refactor: removed validate if email exist

--- lib/utils/validation/validator_email.dart
+++ lib/utils/validation/validator_email.dart
@@ -1,15 +1,7 @@
-import 'package:marketplace/repository/get_store_by_email.dart';
-
 Future<bool> validatorEmail(String email) async {
   final emailRegex = RegExp(
     r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
   );
-
-  final store = await getStoreByEmailRepository(email);
-
-  if (store != null) {
-    throw Exception("O e-mail $email já está em uso!");
-  }
 
   if (!emailRegex.hasMatch(email)) {
     throw Exception("Campo e-mail não é valido");
