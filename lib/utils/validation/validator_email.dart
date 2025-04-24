02378c2b92cf85c7dce6390ac5c23e3b7bb0319e
Juan <juannnerysantos@gmail.com>
2025-04-23 23:20:32 -0300

    feat: validate existing email

--- lib/utils/validation/validator_email.dart
+++ lib/utils/validation/validator_email.dart
@@ -1,10 +1,14 @@
-bool validatorEmail(String email) {
+import 'package:marketplace/repository/get_store_by_email.dart';
+
+Future<bool> validatorEmail(String email) async {
   final emailRegex = RegExp(
     r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
   );
 
-  if (email == "") {
-    throw Exception("Campo e-mail não preenchido");
+  final store = await getStoreByEmailRepository(email);
+
+  if (store != null) {
+    throw Exception("O e-mail $email já está em uso!");
   }
 
   if (!emailRegex.hasMatch(email)) {
