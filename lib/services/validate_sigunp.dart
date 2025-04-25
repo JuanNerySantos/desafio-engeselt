4487e834db0f6068fe6f9388d7aea6af5ff2db61
Juan <juannnerysantos@gmail.com>
2025-04-25 13:24:01 -0300

    feat: implement saveEmailLogged in signup

--- lib/services/validate_sigunp.dart
+++ lib/services/validate_sigunp.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:marketplace/repository/get_store_by_email.dart';
 import 'package:marketplace/repository/insert_store.dart';
 import 'package:marketplace/utils/error/saffold_messenger_error.dart';
+import 'package:marketplace/utils/save_email_logged.dart';
 import 'package:marketplace/utils/validation/validator_email.dart';
 import 'package:marketplace/utils/validation/validator_password.dart';
 import 'package:marketplace/utils/validation/viladator_field.dart';
@@ -86,6 +87,7 @@ class ValidateSigunpService {
         phone: phone.toString(),
         selectedState: selectedState.toString(),
       );
+      await saveEmailLogged(email);
 
       await insertdb.insertStoreDb();
 
