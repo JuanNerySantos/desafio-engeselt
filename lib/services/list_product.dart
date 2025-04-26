ba62f0374f30521c8859d0321d9b26bb0fceb455
Juan <juannnerysantos@gmail.com>
2025-04-26 20:08:04 -0300

    refactor: to send store and product in ListProductService

--- lib/services/list_product.dart
+++ lib/services/list_product.dart
@@ -3,32 +3,52 @@ import 'package:marketplace/repository/get_all_product.dart';
 import 'package:marketplace/repository/get_store_by_id.dart';
 
 class ListProductService {
-  Future<List<Map<String, dynamic>>> listProductData() async {
+  Future<List<Map<String, dynamic>>> listProductData(
+    String? findProduct,
+  ) async {
     final products = await getAllProductsRepository();
-
-    print('Total products: ${products.length}');
 
     List<Map<String, dynamic>> result = [];
 
     for (var product in products) {
       try {
-        final int id = int.tryParse(product.storeId) ?? 0;
+        if (findProduct == null) {
+          final int id = int.tryParse(product.storeId) ?? 0;
 
-        final StoreDataData? store = await getStoreByIdRepository(id);
+          final StoreDataData? store = await getStoreByIdRepository(id);
 
-        result.add({
-          'name': product.nameProduct,
-          'price': product.price,
-          'image': product.image,
-          'store': {
-            'nameStore': store?.nameStore,
-            'cep': store?.cep,
-            'city': store?.city,
-            'phone': store?.phone,
-            'neighborhood': store?.neighborhood,
-            'street': store?.street,
-          },
-        });
+          result.add({
+            'name': product.nameProduct,
+            'price': product.price,
+            'image': product.image,
+            'store': {
+              'nameStore': store?.nameStore,
+              'cep': store?.cep,
+              'city': store?.city,
+              'phone': store?.phone,
+              'neighborhood': store?.neighborhood,
+              'street': store?.street,
+            },
+          });
+        } else if (product.nameProduct.contains(findProduct)) {
+          final int id = int.tryParse(product.storeId) ?? 0;
+
+          final StoreDataData? store = await getStoreByIdRepository(id);
+
+          result.add({
+            'name': product.nameProduct,
+            'price': product.price,
+            'image': product.image,
+            'store': {
+              'nameStore': store?.nameStore,
+              'cep': store?.cep,
+              'city': store?.city,
+              'phone': store?.phone,
+              'neighborhood': store?.neighborhood,
+              'street': store?.street,
+            },
+          });
+        }
       } catch (e) {
         print('Erro ao processar o produto ${product.nameProduct}: $e');
       }
