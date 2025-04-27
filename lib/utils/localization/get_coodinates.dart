67f663d28754e6ecf6605ae4bb159fc931e2edb5
Juan <juannnerysantos@gmail.com>
2025-04-27 12:12:36 -0300

    refactor: return the getCoordinatesFromCep

--- lib/utils/localization/get_coodinates.dart
+++ lib/utils/localization/get_coodinates.dart
@@ -1,20 +1,17 @@
 import 'package:geocoding/geocoding.dart';
-import 'package:latlong2/latlong.dart';
 
-Future<LatLng?> getCoordinatesFromCep(String cep) async {
+Future<String?> getCoordinatesFromCep(String cep) async {
   try {
     List<Location> locations = await locationFromAddress('$cep, Brasil');
 
     if (locations.isNotEmpty) {
       final lat = locations.first.latitude;
       final lng = locations.first.longitude;
-      return LatLng(lat, lng);
+      return '$lat, $lng';
     } else {
-      print('Nenhuma localização encontrada.');
+      throw Exception('Nenhuma localização encontrada.');
     }
   } catch (e) {
-    print('Erro ao obter localização: $e');
+    throw Exception('Erro ao obter localização: $e');
   }
-
-  return null;
 }
