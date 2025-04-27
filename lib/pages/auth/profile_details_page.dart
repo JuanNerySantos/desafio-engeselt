82d954db3d59e39a18f736c3421b02613b051731
Juan <juannnerysantos@gmail.com>
2025-04-27 12:36:16 -0300

    feat: implement getCoordinatesFromCep and searchAdress

--- lib/pages/auth/profile_details_page.dart
+++ lib/pages/auth/profile_details_page.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:marketplace/pages/auth/login_page.dart';
 import 'package:marketplace/services/delete_store.dart';
 import 'package:marketplace/services/update_store.dart';
+import 'package:marketplace/ui/components/password_field.dart';
 import 'package:marketplace/ui/components/text_button_styled.dart';
 import 'package:marketplace/ui/components/text_field_styled.dart';
 import 'package:marketplace/utils/list_states.dart';
+import 'package:marketplace/utils/localization/get_coodinates.dart';
 import 'package:marketplace/utils/mask_phone.dart';
+import 'package:marketplace/utils/search_adress.dart';
 
 class ProfileDetailsPage extends StatefulWidget {
   const ProfileDetailsPage({super.key});
@@ -15,6 +18,14 @@ class ProfileDetailsPage extends StatefulWidget {
 }
 
 class _ProfileDetailsPageState extends State<ProfileDetailsPage> {
+  String? _selectedState;
+
+  List<String> adressList = [];
+  String latLng = '';
+  String streetHint = "Rua";
+  String neighborhoodHint = "Bairro";
+  String cityHint = "Cidade";
+  String stateHint = "Estado";
   final TextEditingController nameStoreController = TextEditingController(
     text: "",
   );
@@ -43,202 +54,232 @@ class _ProfileDetailsPageState extends State<ProfileDetailsPage> {
   Widget build(BuildContext context) {
     String? selectedState;
 
-    return Scaffold(
-      appBar: AppBar(title: Text("Alterar Dados cadastrais")),
-      body: Container(
-        width: double.infinity,
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment(0.8, 1),
-            colors: <Color>[Color(0xfff8bbd0), Color(0xfffff3e0)],
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment(0.8, 1),
+              colors: <Color>[Color(0xfff8bbd0), Color(0xfffff3e0)],
+            ),
           ),
-        ),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(height: 30),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: 30),
+                Text(
+                  "Casdastre-se",
+                  style: TextStyle(fontSize: 30, color: Color(0xff6b4226)),
+                ),
+                const SizedBox(height: 20),
 
-              TextFieldStyled(
-                hintText: "Nome da loja",
-                icon: Icons.store,
-                obscureText: false,
-                controller: nameStoreController,
-              ),
-              const SizedBox(height: 20),
+                TextFieldStyled(
+                  controller: nameStoreController,
+                  hintText: "Nome da loja",
+                  icon: Icons.store,
+                  obscureText: false,
+                ),
+                const SizedBox(height: 20),
 
-              TextFieldStyled(
-                hintText: "E-mail",
-                icon: Icons.store,
-                obscureText: false,
-                controller: emailController,
-              ),
-              const SizedBox(height: 20),
+                TextFieldStyled(
+                  hintText: "E-mail",
+                  icon: Icons.email,
+                  obscureText: false,
+                  controller: emailController,
+                ),
+                const SizedBox(height: 20),
 
-              TextFieldStyled(
-                hintText: "Latitude e longitude",
-                icon: Icons.explore,
-                obscureText: false,
-                controller: latlongController,
-              ),
-              const SizedBox(height: 20),
+                TextFieldStyled(
+                  hintText: "CEP",
+                  icon: Icons.place,
+                  obscureText: false,
+                  controller: cepController,
+                  onChanged: (cepController) async {
+                    if (cepController.length == 8) {
+                      String adress = await searchAdress(cepController);
+                      adressList = adress.split(',');
+                      final latLngExist = await getCoordinatesFromCep(
+                        cepController,
+                      );
+                      latLng = latLngExist!;
 
-              TextFieldStyled(
-                hintText: "CEP",
-                icon: Icons.place,
-                obscureText: false,
-                controller: cepController,
-              ),
-              const SizedBox(height: 20),
+                      setState(() {
+                        if (brazilianStates.contains(adressList[3])) {
+                          _selectedState = adressList[3];
+                          stateHint = adressList[3];
+                        } else {
+                          _selectedState = null;
+                          stateHint = "Estado";
+                        }
 
-              TextFieldStyled(
-                hintText: "Nome da rua(com número)",
-                icon: Icons.edit_location_alt,
-                obscureText: false,
-                controller: streetController,
-              ),
-              const SizedBox(height: 20),
+                        streetHint = adressList[0];
+                        streetController.text = adressList[0];
 
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 30),
+                        neighborhoodController.text = adressList[1];
+                        neighborhoodHint = adressList[1];
 
-                child: DropdownButtonFormField<String>(
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Color(0xB5F4D16F),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30),
-                      borderSide: BorderSide.none,
-                    ),
-                    labelText: "Estado",
-                    prefixIcon: Icon(Icons.map, color: Color(0xff6b4226)),
-                  ),
-                  value: selectedState,
-                  items:
-                      brazilianStates.map((String state) {
-                        return DropdownMenuItem(
-                          value: state,
-                          child: Text(state),
-                        );
-                      }).toList(),
-                  onChanged: (String? newValue) {
-                    setState(() {
-                      selectedState = newValue;
-                    });
+                        cityController.text = adressList[2];
+                        cityHint = adressList[2];
+                      });
+                    }
                   },
                 ),
-              ),
-              const SizedBox(height: 20),
+                const SizedBox(height: 20),
 
-              TextFieldStyled(
-                hintText: "Cidade",
-                icon: Icons.location_city,
-                obscureText: false,
-                controller: cityController,
-              ),
-              const SizedBox(height: 20),
+                TextFieldStyled(
+                  hintText: streetHint,
+                  icon: Icons.edit_location_alt,
+                  obscureText: false,
+                  controller: streetController,
+                ),
+                const SizedBox(height: 20),
 
-              TextFieldStyled(
-                hintText: "Bairro",
-                icon: Icons.location_city,
-                obscureText: false,
-                controller: neighborhoodController,
-              ),
-              const SizedBox(height: 20),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 30),
 
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 30),
-                child: TextField(
-                  controller: controllerPhone,
-                  keyboardType: TextInputType.phone,
-                  inputFormatters: [phoneMask],
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Color(0xB5F4D16F),
-                    hintText: "Telefone",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30),
-                      borderSide: BorderSide.none,
+                  child: DropdownButtonFormField<String>(
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Color(0xB5F4D16F),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: BorderSide.none,
+                      ),
+                      labelText: stateHint,
+                      prefixIcon: Icon(Icons.map, color: Color(0xff6b4226)),
                     ),
-                    prefixIcon: Icon(Icons.phone, color: Color(0xff6b4226)),
+                    value: _selectedState,
+                    items:
+                        brazilianStates.map((String state) {
+                          return DropdownMenuItem(
+                            value: state,
+                            child: Text(state),
+                          );
+                        }).toList(),
+                    onChanged: (newValue) {
+                      setState(() {
+                        _selectedState = newValue;
+                      });
+                    },
                   ),
                 ),
-              ),
-              const SizedBox(height: 20),
+                const SizedBox(height: 20),
 
-              TextFieldStyled(
-                hintText: "Senha",
-                icon: Icons.lock,
-                suffixIcon: Icons.visibility,
-                obscureText: true,
-                controller: passwordController,
-              ),
-              const SizedBox(height: 20),
+                TextFieldStyled(
+                  hintText: cityHint,
+                  icon: Icons.location_city,
+                  obscureText: false,
+                  controller: cityController,
+                ),
+                const SizedBox(height: 20),
 
-              Container(
-                padding: EdgeInsets.all(10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    TextButtonStyled(
-                      buttonName: "Alterar",
-                      color: Color(0xffc8e6c9),
-                      onPressed: () async {
-                        final update = UpdateStoreService(
-                          context: context,
-                          cep: cepController.text,
-                          city: cityController.text,
-                          email: emailController.text,
-                          nameStore: nameStoreController.text,
-                          latlong: latlongController.text,
-                          neighborhood: neighborhoodController.text,
-                          password: passwordController.text,
-                          phone: controllerPhone.text,
-                          selectedState: selectedState,
-                          street: streetController.text,
-                        );
-                        await update.updateStore();
-                      },
+                TextFieldStyled(
+                  hintText: neighborhoodHint,
+                  icon: Icons.location_city,
+                  obscureText: false,
+                  controller: neighborhoodController,
+                ),
+                const SizedBox(height: 20),
+
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 30),
+                  child: TextField(
+                    controller: controllerPhone,
+                    keyboardType: TextInputType.phone,
+                    inputFormatters: [phoneMask],
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Color(0xB5F4D16F),
+                      hintText: "Telefone",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: BorderSide.none,
+                      ),
+                      prefixIcon: Icon(Icons.phone, color: Color(0xff6b4226)),
                     ),
+                  ),
+                ),
+                const SizedBox(height: 20),
 
-                    InkWell(
-                      onTap: () async {
-                        final delete = DeleteStoreService(context: context);
+                PasswordField(
+                  hintText: "Senha",
+                  controller: passwordController,
+                ),
+                const SizedBox(height: 20),
 
-                        final storeIsDeleted = await delete.deleteStore();
+                PasswordField(
+                  hintText: "Confirmar seha",
+                  controller: confirmPasswordController,
+                ),
+                const SizedBox(height: 20),
 
-                        if (storeIsDeleted == true) {
-                          setState(() {
-                            Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) {
-                                  return LoginPage();
-                                },
-                              ),
-                            );
-                          });
-                        }
-                      },
-                      child: Container(
-                        padding: EdgeInsets.all(8),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      TextButtonStyled(
+                        buttonName: "Alterar",
+                        color: Color(0xffc8e6c9),
+                        onPressed: () async {
+                          final update = UpdateStoreService(
+                            context: context,
+                            cep: cepController.text,
+                            city: cityController.text,
+                            email: emailController.text,
+                            nameStore: nameStoreController.text,
+                            latlong: latlongController.text,
+                            neighborhood: neighborhoodController.text,
+                            password: passwordController.text,
+                            phone: controllerPhone.text,
+                            selectedState: selectedState,
+                            street: streetController.text,
+                          );
+                          await update.updateStore();
+                        },
+                      ),
 
-                        decoration: BoxDecoration(
-                          color: Colors.red,
-                          borderRadius: BorderRadius.circular(40),
-                        ),
-                        child: Text(
-                          'Excluir conta',
-                          style: const TextStyle(fontSize: 20),
+                      InkWell(
+                        onTap: () async {
+                          final delete = DeleteStoreService(context: context);
+
+                          final storeIsDeleted = await delete.deleteStore();
+
+                          if (storeIsDeleted == true) {
+                            setState(() {
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) {
+                                    return LoginPage();
+                                  },
+                                ),
+                              );
+                            });
+                          }
+                        },
+                        child: Container(
+                          padding: EdgeInsets.all(8),
+
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(40),
+                          ),
+                          child: Text(
+                            'Excluir conta',
+                            style: const TextStyle(fontSize: 20),
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
 
-              SizedBox(height: 20),
-            ],
+                SizedBox(height: 20),
+              ],
+            ),
           ),
         ),
       ),
