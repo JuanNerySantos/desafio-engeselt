9b1c3b269111447e72d6113d639ff807b7d37096
Juan <juannnerysantos@gmail.com>
2025-04-27 15:46:52 -0300

    feat: add ResetPasswordService

---
+++ lib/services/reset_password.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:marketplace/data/drift/db_connection/app_db.dart';
+import 'package:marketplace/repository/get_store_by_email.dart';
+import 'package:marketplace/repository/update_store.dart';
+import 'package:marketplace/utils/error/saffold_messenger_error.dart'
+    show messageError;
+import 'package:marketplace/utils/validation/validator_password.dart';
+
+class ResetPasswordService {
+  final BuildContext context;
+  final String email;
+  final String newPassword;
+
+  const ResetPasswordService({
+    required this.context,
+    required this.email,
+    required this.newPassword,
+  });
+
+  Future<bool> resetPassword() async {
+    try {
+      final emailExist = await getStoreByEmailRepository(email);
+      validatorPassword(newPassword);
+
+      if (emailExist != null) {
+        updateStoreRepository(
+          AppDb(),
+          logged: '',
+          cep: '',
+          city: '',
+          email: '',
+          latlong: '',
+          nameStore: '',
+          neighborhood: '',
+          password: newPassword,
+          phone: '',
+          selectedState: '',
+          street: '',
+        );
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Alteração feita com sucxesso!'),
+            backgroundColor: Colors.green,
+            duration: Duration(seconds: 4),
+          ),
+        );
+        return true;
+      }
+      throw Exception('E-mail não existe');
+    } catch (e) {
+      messageError(e.toString(), context);
+      return false;
+    }
+  }
+}
